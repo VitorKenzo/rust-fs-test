ce5c7c005c75d3eb3ab5c11fb5fbc63e4a8eeb6f
Vitor Pellegatti <vkenzofp@gmail.com>
2023-07-23 02:45:00 -0300

    Example of good reading from a file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::{fs, io::{Write, BufWriter}};
+use std::{fs, fs::File, io::{Write, BufWriter, BufReader, BufRead}, str::from_utf8};
 
-fn write_data_into_file(path: &str, data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+
+const BUFFER_SIZE: usize = 512;
+
+fn write_data_into_file(path: &str, data: &[u8]) -> std::io::Result<()> {
 
     let file = fs::OpenOptions::new()
         .create(true)
@@ -17,11 +20,36 @@ fn write_data_into_file(path: &str, data: &[u8]) -> Result<(), Box<dyn std::erro
     Ok(())
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
+
+    // using the writing function we created
     match write_data_into_file(&".\\test.log", &"Testing the test \nwahooooo!!\n".as_bytes()) {
-        Ok(_) => print!("Sucessfully wrote in the file!"),
-        Err(_) => println!("Something went wrong opsies :<")
+        Ok(_) => println!("Sucessfully wrote in the file!"),
+        Err(e) => println!("Something went wrong opsies :< {:?}", e)
     }
 
 
+    let path = ".\\test.log";
+
+    let file = File::open(&path)?;
+    let mut reader = BufReader::with_capacity(BUFFER_SIZE, file);
+
+    println!("Reading {}...", path);
+    loop {
+        let buffer = reader.fill_buf()?;
+        let buffer_length = buffer.len();
+
+        //lets say its over, we can get out of the loop
+        if buffer_length == 0 {
+            break;
+        }
+
+        //do something part
+        print!("{}\n", from_utf8(&buffer).unwrap());
+
+        //making sure we are not stuck
+        reader.consume(buffer_length);
+    }
+
+    Ok(())
 }
